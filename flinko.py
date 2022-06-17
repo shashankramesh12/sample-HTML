0c4051f50e1a133e2e3d15d10eb177fe217f0364
shashankramesh12 <79041821+shashankramesh12@users.noreply.github.com>
2022-06-17 12:37:56 +0530

    Update flinko.py

--- flinko.py
+++ flinko.py
@@ -23,7 +23,7 @@ def login(mail, password):
     token = tk['responseObject']['access_token']
     head["Authorization"] = "Bearer " + token
 
-    suiteid = 'SUITE1004'
+    suiteid = 'SUITE1003'
     pes = s.post('https://app.flinko.com:8109/optimize/v1/dashboard/execution/suite/' + suiteid, headers=head)
     out = json.loads(pes.content)
     exid = out['responseObject']['id']
